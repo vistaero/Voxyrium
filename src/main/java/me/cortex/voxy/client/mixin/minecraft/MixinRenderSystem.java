b7e4c5f249fb9981a7d6b51b6bf229ab59780ab7
vistaero <vistaero@outlook.com>
2026-08-25 21:05:19 +0200

    Defer OpenGL initialization until renderer setup

--- src/main/java/me/cortex/voxy/client/mixin/minecraft/MixinRenderSystem.java
+++ src/main/java/me/cortex/voxy/client/mixin/minecraft/MixinRenderSystem.java
@@ -1,7 +1,6 @@
 package me.cortex.voxy.client.mixin.minecraft;
 
 
-import com.mojang.blaze3d.systems.GpuDevice;
 import com.mojang.blaze3d.systems.RenderSystem;
 import me.cortex.voxy.client.VoxyClient;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinRenderSystem {
     //We need to inject before iris to initalize our systems
     @Inject(method = "initRenderer", order = 900, remap = false, at = @At("RETURN"))
-    private static void voxy$injectInit(GpuDevice device, CallbackInfo ci) {
-        VoxyClient.initVoxyClient(device);
+    private static void voxy$injectInit(int debugVerbosity, boolean synchronous, CallbackInfo ci) {
+        VoxyClient.initVoxyClient();
     }
 }
